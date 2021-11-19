b3b9088272e2fdf7e4e4b24ed80801fb0c6d47f0
erwinhenraat <e.henraat@ma-web.nl>
2021-11-19 23:07:59 +0100

    messing with rotation - not working yet

--- game.js
+++ game.js
@@ -9,17 +9,76 @@ let size = 10;
 let previous = 0;
 let delta = 0;
 
+
+let horInput = 0;
+let verInput = 0;
+
+let acc = 0.001;
+let speed = 0;
+let maxSpeed = 0.2;
+
+let angle = 0;
+let rotSpeed = 0;
+let rotAcc = 0.001;
+//onpress
+document.addEventListener("keydown", (event)=>{
+    if(event.key == "w"){
+        verInput = 1;
+    }
+    else if(event.key == "s"){
+        verInput = -1;
+    }
+    else if(event.key == "a"){
+        horInput = -1;
+    }
+    else if(event.key == "d"){
+        horInput = 1;
+    }
+});
+//onrelease
+document.addEventListener("keyup", (event)=>{
+    if(event.key == "w"){
+        verInput = 0;
+    }
+    else if(event.key == "s"){
+        verInput = 0;
+    }
+    else if(event.key == "a"){
+        horInput = 0;
+    }
+    else if(event.key == "d"){
+        horInput = 0;
+    }
+});
+
 //gameloop
 function update(time){
-        
     if(previous != 0){
         delta = time - previous
     }
+    //check input for speed
+    if(speed < maxSpeed)speed += verInput * acc;
+    if(speed < 0)speed = 0;
+    
+    //check input for rotation
+    if(horInput!=0)rotSpeed -= rotAcc * horInput;
+    if(horInput==0 && rotSpeed !=0)rotSpeed -= rotAcc * horInput;
 
+  
+    angle += rotSpeed;
+    console.log("rspeed" + rotSpeed);
+
+    ctx.rotate(rotSpeed*Math.PI/2);
+    //verplaats pivot
+    //ctx.translate(x,y);
+    
+    //ctx.translate(0,0);
+
+   
 
     ctx.clearRect(0,0,canvas.width, canvas.height);
     //beweging
-    x+= 0.08 * delta;
+    x+= speed * delta;
     //teken vierkant
     drawShip(x,y,size,"red");
 
