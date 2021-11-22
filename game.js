b88cc70b1ed8c28cd3fe96b243f1ee80854515cc
erwinhenraat <e.henraat@ma-web.nl>
2021-11-22 13:57:48 +0100

    rotation of multiple objects w.i.p

--- game.js
+++ game.js
@@ -47,26 +47,17 @@ document.addEventListener("keyup", (event)=>{
 let angle = 0;
 let rotSpeed = 0.05;
 
-
 //!!!!!!To do: add all rotation values to offset every next rotation
-//let rockPointModifiers = [];
+
 let rocks = [];
-
-function addRock(xPos, yPos, size){
-    rocks.push({x:xPos, y:yPos, size:size, rot:Math.random()*Math.PI*2 , rotSpeed:-(Math.random()*Math.PI) + (Math.random()*Math.PI*2), modifiers:generateRandomRockPointModifiers()});
-    
-}
-
 addRock(200,100, 25);
 addRock(200,200, 25);
+//addRock(200,300, 35);
 
-//console.log(rocks);
-rocks.forEach(r => {
-    drawRock(r.x,r.y,r.size,"grey", r.modifiers);    
-});
-
-
-
+function addRock(xPos, yPos, size){
+    rocks.push({x:xPos, y:yPos, size:size, rot:Math.random()*Math.PI*2 , rotSpeed:-(Math.random()*Math.PI/8) + (Math.random()*Math.PI/4), modifiers:generateRandomRockPointModifiers()});
+    
+}
 function generateRandomRockPointModifiers(){
     return [
         {y:1+Math.random()*1},                          //upper point modifier
@@ -76,14 +67,9 @@ function generateRandomRockPointModifiers(){
         {x:2+Math.random()*4, y:2+Math.random()*4},
     ];
 }
-
-
 function drawRock(x,y,size, color, modifiers){
     //Rots tekenen
-    if(size < 30){
-        console.warn("Rocksize below 30 may result in invisible Rocks when spawned @ player position");
-    }
-
+   
     ctx.fillStyle = color;
     ctx.beginPath();    
     ctx.moveTo(x,y-size/modifiers[0].y);
@@ -112,9 +98,31 @@ function update(time){
     x+= speed * delta;
     
 
+    //Tel alle rotaties van alle objecten bij elkaar op
+
+    //roteer elk object en trek de rotatie eraf
+    //toteer volgende object met overgebleven rotatie
+
+    let totalRot = 0;//rotSpeed*inputVector.x;
+    rocks.forEach(r => {
+        totalRot += r.rotSpeed;
+    });
+    console.log("total rot" + totalRot);
+
+    rocks.forEach(r => {
+        ctx.translate(r.x,r.y);
+        ctx.clearRect(0-r.size,0-r.size,r.size*2, r.size*2);
+        drawRock(0,0,r.size,"grey",r.modifiers);
+        ctx.rotate(totalRot);
+        ctx.translate(-r.x,-r.y);
+        totalRot -= r.rotSpeed;
+        console.log("between:" +totalRot);
+
+    });
+    console.log("after loop:" +totalRot);
 
     //verplaats origin van canvas naar locatie van het schip
-    
+/*
     ctx.translate(x,y);
     ctx.clearRect(0-size,0-size,size*2, size*2);
     drawShip(0,0,size,"purple");
@@ -123,11 +131,9 @@ function update(time){
     
     //verplaats origin van canvas terug naar de hoek
     ctx.translate(-x,-y);
-
+*/
     //rock
     
-
-
 
   
 
