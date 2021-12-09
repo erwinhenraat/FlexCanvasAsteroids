a8073ba73dd95a20ccc53b06ff4233381d49fb9b
erwinhenraat <e.henraat@ma-web.nl>
2021-12-09 15:03:59 +0100

    ship object created, rock movement, teleportation

--- game.js
+++ game.js
@@ -1,18 +1,6 @@
+//haal canvas en context op
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
-
-//schip data
-let x = 100;
-let y = 100;
-let size = 10;
-let rotation = 0;
-let rotSpeed = 0.05;
-let acc = 0.001;
-let speed = 0;
-let maxSpeed = 0.2;
-
-
-
 
 //timestamp vorige frame en delta (verschil)
 let previous = 0;
@@ -21,10 +9,7 @@ let delta = 0;
 //controller input
 let inputVector = {x:0,y:0};
 
-
-
-
-//onpress
+//Bijhouden van ingedrukte toetsen
 document.addEventListener("keydown", (event)=>{
     if(event.key == "w"){
         inputVector.y = 1;
@@ -39,7 +24,7 @@ document.addEventListener("keydown", (event)=>{
         inputVector.x = 1;
     }
 });
-//onrelease
+//bij loslaten
 document.addEventListener("keyup", (event)=>{
     if(event.key == "w" || event.key =="s"){
         inputVector.y = 0;
@@ -49,40 +34,36 @@ document.addEventListener("keyup", (event)=>{
     }
 });
 
-
-
-
+//maak schip object
+let ship = {
+    x:100,
+    y:100,
+    size:10,
+    rotation:0,
+    rotSpeed:0.05,
+    acc:0.001,
+    speed:0,
+    maxSpeed:0.2
+};
+//Maak Rotsen
 let rocks = [];
 addRock(200,100, 20, "red");
 addRock(200,200, 25, "blue");
 addRock(200,300, 35, "pink");
 
 function addRock(xPos, yPos, size, color){
-    rocks.push({x:xPos, y:yPos, size:size,color:color, rotation:Math.random()*Math.PI*2 , rotSpeed:-(Math.random()*Math.PI/8) + (Math.random()*Math.PI/4), modifiers:generateRandomRockPointModifiers()});
-    
+    rocks.push({
+        x:xPos, 
+        y:yPos,        
+        size:size,
+        color:color, 
+        direction:{x:Math.random()*4-2,y:Math.random()*4-2},
+        rotation:Math.random()*Math.PI*2, 
+        rotSpeed:-(Math.random()*Math.PI/16) + (Math.random()*Math.PI/16), 
+        modifiers:createRockShapeModifiers()
+    });    
 }
-function generateRandomRockPointModifiers(){
-    return [
-        {y:1+Math.random()*1},                          //upper point modifier
-        {x:2+Math.random()*4, y:2+Math.random()*4},     //other point modifier
-        {x:2+Math.random()*4, y:2+Math.random()*4},
-        {x:2+Math.random()*4, y:2+Math.random()*4},
-        {x:2+Math.random()*4, y:2+Math.random()*4},
-    ];
-}
-function drawRock(x,y,size, color, modifiers){
-    //Rots tekenen
-   
-    ctx.fillStyle = color;
-    ctx.beginPath();    
-    ctx.moveTo(x,y-size/modifiers[0].y);
-    ctx.lineTo(x+size/modifiers[1].x,y-size/modifiers[1].y);
-    ctx.lineTo(x+size/modifiers[2].x,y+size/modifiers[2].y);
-    ctx.lineTo(x-size/modifiers[3].x,y+size/modifiers[3].y);
-    ctx.lineTo(x-size/modifiers[4].x,y-size/modifiers[4].y);
-    ctx.lineTo(x,y-size/modifiers[0].y);
-    ctx.fill();
-}
+
 //gameloop
 function update(time){       
    
@@ -93,84 +74,46 @@ function update(time){
     }    
     
     //check input for speed
-    if(speed < maxSpeed)speed += inputVector.y * acc;
-    if(speed < 0)speed = 0;
-    if(speed > 0 && inputVector.y == 0) speed-=0.001;
+    if(ship.speed < ship.maxSpeed)ship.speed += inputVector.y * ship.acc;
+    if(ship.speed < 0)ship.speed = 0;
+    if(ship.speed > 0 && inputVector.y == 0) ship.speed-=0.001;
     
-    //beweging
-    //x+= speed * delta;
- 
-    
-    rotation += rotSpeed * inputVector.x;
+    //beweging         
+    ship.rotation += ship.rotSpeed * inputVector.x;
+    ship.x += Math.cos(ship.rotation)*ship.speed*delta;
+    ship.y += Math.sin(ship.rotation)*ship.speed*delta;
 
-    x += Math.cos(rotation)*speed*delta;
-    y += Math.sin(rotation)*speed*delta;
-       
+    //terleporteer schip
+    if(ship.x > canvas.width+ship.size)ship.x = -ship.size;  
+    if(ship.x < -ship.size)ship.x = canvas.width+ship.size;
+    if(ship.y > canvas.height+ship.size)ship.y = -ship.size;
+    if(ship.y < -ship.size)ship.y = canvas.height + ship.size;
 
-    ctx.translate(x,y);
-    ctx.rotate(rotation);
-
+    ctx.translate(ship.x,ship.y);
+    ctx.rotate(ship.rotation);
     drawShip(0,0,10,"blue");
-
     ctx.resetTransform();
  
+    rocks.forEach(r => {    
+        r.x += r.direction.x;
+        r.y += r.direction.y;
 
-
-
-    rocks.forEach(r => {
-
-        //Translaties moeten ook bij elkaar worden opgeteld net als bij total Rotation
-       
+        //teleporteer rotsen
+        if(r.x > canvas.width + r.size)r.x = -r.size;
+        if(r.x < - r.size)r.x = canvas.width + r.size;
+        if(r.y > canvas.height + r.size)r.y = -r.size;
+        if(r.y < - r.size)r.y = canvas.height + r.size;
+        
         ctx.translate(r.x,r.y);        
-       
-        r.rotation += r.rotSpeed;
-      
+        r.rotation += r.rotSpeed;      
         ctx.rotate(r.rotation);
-        drawRock(0,0,r.size,r.color,r.modifiers);
-        
-        ctx.resetTransform();
-        
+        drawRock(0,0,r.size,r.color,r.modifiers);        
+        ctx.resetTransform();        
     });
-
-
-
-    //verplaats origin van canvas naar locatie van het schip
-/*
-    ctx.translate(x,y);
-    ctx.clearRect(0-size,0-size,size*2, size*2);
-    drawShip(0,0,size,"purple");
-    //roteer de canvas terug
-    ctx.rotate((rotSpeed*inputVector.x)*Math.PI/2);
-    
-    //verplaats origin van canvas terug naar de hoek
-    ctx.translate(-x,-y);
-*/
-    //rock
-    
-
-  
-
-    previous = time;
-  
-    window.requestAnimationFrame(update);
-  
+    previous = time;  
+    window.requestAnimationFrame(update);  
 }
 window.requestAnimationFrame(update);
-
-function drawSquare(x,y,size, color){
-    //Vierkantje tekenen
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(x,y);
-    ctx.lineTo(x+size,y);
-    ctx.lineTo(x+size,y+size);
-    ctx.lineTo(x,y+size);
-    ctx.lineTo(x,y);
-    ctx.fill();
-}
-
-
-
 
 function drawShip(x,y,size, color){
     //schip tekenen
@@ -181,4 +124,26 @@ function drawShip(x,y,size, color){
     ctx.lineTo(x-size/2,y+size/2);    
     ctx.lineTo(x+size/2,y);
     ctx.fill();
+}
+function drawRock(x,y,size, color, modifiers){
+    //Rots tekenen met 5 hoeken   
+    ctx.fillStyle = color;
+    ctx.beginPath();    
+    ctx.moveTo(x,y-size/modifiers[0].y);
+    ctx.lineTo(x+size/modifiers[1].x,y-size/modifiers[1].y);
+    ctx.lineTo(x+size/modifiers[2].x,y+size/modifiers[2].y);
+    ctx.lineTo(x-size/modifiers[3].x,y+size/modifiers[3].y);
+    ctx.lineTo(x-size/modifiers[4].x,y-size/modifiers[4].y);
+    ctx.lineTo(x,y-size/modifiers[0].y);
+    ctx.fill();
+}
+function createRockShapeModifiers(){
+    //5 random hoekpunten voor de rotsen 
+    return [
+        {y:1+Math.random()*1},                          //upper point modifier
+        {x:2+Math.random()*4, y:2+Math.random()*4},     //other point modifier
+        {x:2+Math.random()*4, y:2+Math.random()*4},
+        {x:2+Math.random()*4, y:2+Math.random()*4},
+        {x:2+Math.random()*4, y:2+Math.random()*4},
+    ];
 }
